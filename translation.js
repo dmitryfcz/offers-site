2719dfae01f4cf0f594ae47cb85fef627cea35c3
dmitryfcz <43968889+dmitryfcz@users.noreply.github.com>
2022-07-23 21:43:23 +0300

    Add files via upload

---
+++ translation.js
@@ -0,0 +1,21 @@
+var translation = {
+    en: {
+        phrase1: 'Choose which option you are interested in:',
+        phrase2: 'Make money',
+        phrase3: 'Free training. Minimum deposit 100$.',
+        phrase4: 'Gambling',
+        phrase5: 'Free spins. Without deposit.',
+        phrase6: 'Online dating',
+        phrase7: 'Meet a friend for a long term relationship or fun.',
+        phrase8: 'Sweepstakes',
+        phrase9: 'Win gifts and coupons from famous stores and brands up to 500$.',
+        phrase10: 'Your gender:',
+        phrase11: 'Male',
+        phrase12: 'Female',
+        phrase13: 'Your age:',
+        phrase14: 'Your phone: *',
+        phrase15: 'Your email: *',
+        email: 'Your email',
+        button: 'Get offers',
+    }
+}
